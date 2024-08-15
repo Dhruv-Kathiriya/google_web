3fbfc5645fbd5286e5fb39078ed8d68127e1ddeb
Taksh <dhruvkathiriya933@gnmail.com>
2024-08-15 19:52:38 +0530

    Chorme_web

---
+++ lib/utilis/lists.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> list1 = [
+  {
+    'image':
+        "https://static.vecteezy.com/system/resources/previews/027/179/391/original/google-sheet-spreadsheet-icon-logo-symbol-free-png.png",
+    'name': " Sheets",
+  },
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png ",
+    'name': "Home",
+  },
+  {
+    'image': "https://cdn-icons-png.flaticon.com/512/4438/4438839.png",
+    'name': "Notepad",
+  },
+  {
+    'image':
+        "https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-user-icon-png-image_1796659.jpg",
+    'name': "Login",
+  },
+];
+List<Map<String, dynamic>> list2 = [
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png",
+    'name': "YouTube",
+  },
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png ",
+    'name': "Telegarm",
+  },
+  {
+    'image':
+        "https://upload.wikimedia.org/wikipedia/commons/1/13/ChatGPT-Logo.png",
+    'name': "Chatgpt",
+  },
+];
